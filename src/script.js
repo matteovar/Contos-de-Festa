4df7715b21f5673d232c56e0855d12469e76a5fc
matteovar <matvarnier@gmail.com>
2025-08-25 09:06:07 -0300

    Funcao de seta adicionada, esta mudando as imagens corretamente JavaScript adicionado

---
+++ src/script.js
@@ -0,0 +1,41 @@
+document.querySelectorAll(".box").forEach((box) => {
+  const themes = {
+    boy: [
+      { img: "assets/boy_theme.svg", title: "Tema de urso" },
+      { img: "assets/boy_theme2.png", title: "Tema de carros" },
+      { img: "assets/boy_theme3.png", title: "Tema de dinossauro" },
+    ],
+    neutral: [
+      { img: "assets/neutral_theme.svg", title: "Tema de urso" },
+      { img: "assets/neutral_theme2.png", title: "Tema de natureza" },
+    ],
+    girl: [
+      { img: "assets/girl_theme.svg", title: "Tema de princesa" },
+      { img: "assets/girl_theme2.png", title: "Tema de flores" },
+      { img: "assets/girl_theme3.png", title: "Tema de unicórnio" },
+    ],
+  };
+
+  const themeType = box.dataset.theme;
+  const themeData = themes[themeType];
+  let currentIndex = 0;
+
+  // Seleciona a imagem principal (não a imagem do botão)
+  const imgElement = box.querySelector("figure img:not(.btn img)");
+  const titleElement = box.querySelector("h3");
+
+  box.querySelector(".btn-left").addEventListener("click", () => {
+    currentIndex = (currentIndex - 1 + themeData.length) % themeData.length;
+    updateTheme();
+  });
+
+  box.querySelector(".btn-right").addEventListener("click", () => {
+    currentIndex = (currentIndex + 1) % themeData.length;
+    updateTheme();
+  });
+
+  function updateTheme() {
+    imgElement.src = themeData[currentIndex].img;
+    titleElement.textContent = themeData[currentIndex].title;
+  }
+});
